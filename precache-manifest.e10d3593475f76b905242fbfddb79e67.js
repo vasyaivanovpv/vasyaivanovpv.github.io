cd85241ecb9788e20aa9a0773eb9b35fe650adbf
Vasya Ivanov <soldatik2005@mail.ru>
2019-10-26 17:05:33 +0300

    init build

---
+++ precache-manifest.e10d3593475f76b905242fbfddb79e67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfec9966fe65335aa2a1fab40a11f7ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daf30c4a4271541f2c97",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "14f4d17189b24d50c8fb",
+    "url": "/static/js/2.308a7ab3.chunk.js"
+  },
+  {
+    "revision": "daf30c4a4271541f2c97",
+    "url": "/static/js/main.0ac4da93.chunk.js"
+  },
+  {
+    "revision": "8e1dd7fd06d4903153b9",
+    "url": "/static/js/runtime-main.529c3501.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
